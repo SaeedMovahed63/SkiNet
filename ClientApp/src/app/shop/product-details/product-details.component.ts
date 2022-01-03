06c673f968a9f154aba7613333cb44402a310548
Saeed Movahed <saeed.movahed@live.com>
2022-01-03 15:09:32 +0330

    pazzazz

--- ClientApp/src/app/shop/product-details/product-details.component.ts
+++ ClientApp/src/app/shop/product-details/product-details.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
-  product:IProduct;
+  product: IProduct;
   constructor(
-    private shopService:ShopService , 
-    private activateRoot : ActivatedRoute) 
-    {
+    private shopService: ShopService,
+    private activateRoot: ActivatedRoute,
+    private bcService: BreadcrumbService
+  ) {
     this.product = {} as IProduct;
-   }
+  }
 
   ngOnInit() {
     this.loadProduct();
   }
 
-  loadProduct(){
-    this.shopService.getProduct(+this.activateRoot.snapshot.paramMap.get('id')!).subscribe(product =>{
-      this.product = product;
-    },error =>{
-      console.log(error)
-    });
+  loadProduct() {
+    this.shopService
+      .getProduct(+this.activateRoot.snapshot.paramMap.get('id')!)
+      .subscribe(
+        (product) => {
+          this.product = product;
+          this.bcService.set('@ProductDetails', product.name);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
-
 }
--- ClientApp/src/app/shop/shop-routing.module.ts
+++ ClientApp/src/app/shop/shop-routing.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShopComponent } from './shop.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-const routes:Routes=[
-  {path:'',component:ShopComponent},
-  {path:':id',component:ProductDetailsComponent},
+const routes: Routes = [
+  { path: '', component: ShopComponent },
+  {
+    path: ':id',
+    component: ProductDetailsComponent,
+    data: { breadcrumb: { alias: 'ProductDetails' } },
+  },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forChild(routes)
-  ],exports:[RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class ShopRoutingModule { }
+export class ShopRoutingModule {}
