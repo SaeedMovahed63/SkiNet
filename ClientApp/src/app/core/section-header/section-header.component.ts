03e06b3e59ada8a439d5d5a843c15b6fa60d6d73
Saeed Movahed <saeedmovahed63@gmail.com>
2022-01-04 22:39:57 +0330

    -

--- ClientApp/src/app/core/section-header/section-header.component.ts
+++ ClientApp/src/app/core/section-header/section-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-header.component.scss']
 })
 export class SectionHeaderComponent implements OnInit {
+  breadcrumb$!:Observable<any[]>;
+  constructor(private bcService:BreadcrumbService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.breadcrumb$=this.bcService.breadcrumbs$;
   }
 
 }
