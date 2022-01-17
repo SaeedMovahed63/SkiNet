e1ce956b288b5a3d7e05e58014c0840d567a511e
Saeed Movahed <saeedmovahed63@gmail.com>
2022-01-17 21:47:47 +0330

    basket service

--- ClientApp/src/app/basket/basket.service.ts
+++ ClientApp/src/app/basket/basket.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { IBasket } from '../shared/models/basket';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BasketService {
+  baseUrl = environment.apiUrl;
+  private basketSource = new BehaviorSubject<IBasket>(null as any);
+  basket$ = this.basketSource.asObservable();
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getBasket(id: string) {
+    return this.http.get<IBasket>(this.baseUrl + 'basket?id=' + id).pipe(
+      map((basket: IBasket) => {
+        this.basketSource.next(basket);
+      })
+    );
+  }
+
+  setBasket(basket: IBasket) {
+    return this.http.post<IBasket>(this.baseUrl + 'basket', basket).subscribe(
+      (response: IBasket) => {
+        this.basketSource.next(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getCurrentBasketValue(){
+    return this.basketSource.value;
+  }
 }
