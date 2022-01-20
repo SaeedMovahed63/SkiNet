241f12f33e125f12d0ce9e287f38b31bd5ee169e
Saeed Movahed <saeedmovahed63@gmail.com>
2022-01-20 18:39:39 +0330

    --

--- ClientApp/src/app/basket/basket.service.ts
+++ ClientApp/src/app/basket/basket.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { Basket, IBasket, IBasketItem } from '../shared/models/basket';
+import { Basket, IBasket, IBasketItem, IBasketTotals } from '../shared/models/basket';
 import { IProduct } from '../shared/models/product';
 
 @Injectable({
@@ -13,12 +13,17 @@ export class BasketService {
   baseUrl = environment.apiUrl;
   private basketSource = new BehaviorSubject<IBasket>(null as any);
   basket$ = this.basketSource.asObservable();
+  
+  private basketTotalSource = new BehaviorSubject<IBasketTotals>(null as any);
+  basketTotal$ = this.basketTotalSource.asObservable();
+
   constructor(private http: HttpClient) {}
 
   getBasket(id: string) {
     return this.http.get<IBasket>(this.baseUrl + 'basket?id=' + id).pipe(
       map((basket: IBasket) => {
         this.basketSource.next(basket);
+        this.calculateTotals(); 
       })
     );
   }
@@ -27,7 +32,8 @@ export class BasketService {
     return this.http.post<IBasket>(this.baseUrl + 'basket', basket).subscribe(
       (response: IBasket) => {
         this.basketSource.next(response);
-        console.log(response);
+        this.calculateTotals(); 
+        //console.log(response);
       },
       (error) => {
         console.log(error);
@@ -49,6 +55,16 @@ export class BasketService {
     this.setBasket(basket);
   }
 
+  private calculateTotals(){
+    const basket = this.getCurrentBasketValue();
+    const shipping = 0;
+    const subtotal = basket.items.reduce((a,b) =>(b.price * b.quantity) +a ,0);
+    const total= subtotal + shipping;
+    this.basketTotalSource.next({shipping , total , subtotal});
+
+  }
+  
+  
   private addOrUpdateItem(
     items: IBasketItem[],
     itemToAdd: IBasketItem,
