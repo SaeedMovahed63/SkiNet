04a6a79a7633ed6f590fb3506c79e15c3c7b94e1
Saeed Movahed <saeedmovahed63@gmail.com>
2022-02-03 23:45:35 +0330

    -

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using API.Dtos;
+using API.Errors;
+using API.Extensions;
+using AutoMapper;
+using Core.Entities.Identity;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+       
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager
+           )
+        {
+           
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+    
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized(new ApiResponse(401));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "token",
+                DisplayName = user.DisplayName
+            };
+        }
+
+        
+    }
+}
