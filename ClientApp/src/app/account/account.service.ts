b2f450f198a7843a277d5c92ef221a63d23c1f64
Saeed Movahed <saeed.movahed@live.com>
2022-02-12 16:37:20 +0330

    --

--- ClientApp/src/app/account/account.service.ts
+++ ClientApp/src/app/account/account.service.ts
@@ -8,32 +8,32 @@ import { IAddress } from '../shared/models/address';
 import { IUser } from '../shared/models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<IUser>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   loadCurrentUser(token: string) {
     if (token == null) {
       this.currentUserSource.next(null as any);
-      return of(null);
+      return of(null as any);
     }
 
     let headers = new HttpHeaders();
     headers = headers.set('Authorization', `Bearer ${token}`);
 
-    return this.http.get<IUser>(this.baseUrl + 'account', {headers}).pipe(
+    return this.http.get<IUser>(this.baseUrl + 'account', { headers }).pipe(
       map((user: IUser) => {
         if (user) {
           localStorage.setItem('token', user.token);
           this.currentUserSource.next(user);
         }
       })
-    )
+    );
   }
 
   login(values: any) {
@@ -44,18 +44,20 @@ export class AccountService {
           this.currentUserSource.next(user);
         }
       })
-    )
+    );
   }
 
   register(values: any) {
-    return this.http.post<IUser>(this.baseUrl + 'account/register', values).pipe(
-      map((user: IUser) => {
-        if (user) {
-          localStorage.setItem('token', user.token);
-          this.currentUserSource.next(user);
-        }
-      })
-    )
+    return this.http
+      .post<IUser>(this.baseUrl + 'account/register', values)
+      .pipe(
+        map((user: IUser) => {
+          if (user) {
+            localStorage.setItem('token', user.token);
+            this.currentUserSource.next(user);
+          }
+        })
+      );
   }
 
   logout() {
--- ClientApp/src/app/account/login/login.component.ts
+++ ClientApp/src/app/account/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AccountService } from '../account.service';
 @Component({
   selector: 'app-login',
@@ -8,7 +9,7 @@ import { AccountService } from '../account.service';
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private router: Router) {}
 
   ngOnInit() {
     this.createLoginForm();
@@ -16,7 +17,10 @@ export class LoginComponent implements OnInit {
 
   createLoginForm() {
     this.loginForm = new FormGroup({
-      email: new FormControl('', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+      ]),
       password: new FormControl('', Validators.required),
     });
   }
@@ -26,6 +30,7 @@ export class LoginComponent implements OnInit {
     this.accountService.login(this.loginForm.value).subscribe(
       () => {
         console.log('user logged in');
+        this.router.navigateByUrl('/shop');
       },
       (error) => {
         console.log(error);
